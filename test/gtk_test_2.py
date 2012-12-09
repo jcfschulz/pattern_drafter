6bf1184c1dc7409f10aedce253a9943d6b0cc411
Julius Schulz <askan@anor.(none)>
2012-12-09 17:13:16 +0100

    circles remain shape under resize

--- test/gtk_test_2.py
+++ test/gtk_test_2.py
@@ -22,13 +22,16 @@ class Transform(framework.Screen):
         return pointx,pointy
 
 
-    def point(self, point, pointsize=0.01, color="black"):
+    def point(self, point, pointsize=10, color="black"):
         pointx,pointy = self.recalc(point)
         self.ctx.set_source_rgb (colors[color][0],colors[color][1],colors[color][2])
-        self.ctx.move_to(pointx,pointy)
-        self.ctx.arc (pointx, pointy, 0.01, 0., 2*math.pi)
-        self.ctx.set_line_width (2*pointsize)
+        self.ctx.save()
+        self.ctx.translate(pointx, pointy)
+        self.ctx.scale (1/(self.zoom*self.width), 1/(self.zoom*self.height)) 
+        self.ctx.arc (0,0, 1, 0., 2*math.pi)
+        self.ctx.set_line_width (pointsize)
         self.ctx.stroke()
+        self.ctx.restore()
 
     def draw(self, ctx, width, height):
         self.width = float(width)
@@ -41,7 +44,7 @@ class Transform(framework.Screen):
         self.maxwidth = 40.
         self.maxaspect = self.maxwidth/self.maxheight
 
-        ctx.scale (self.zoom*width, self.zoom*height) # Normalizing the canvas
+        ctx.scale (self.zoom*self.width, self.zoom*self.height) # Normalizing the canvas
         ctx.set_source_rgb (1, 1, 1)
         ctx.paint()
 
