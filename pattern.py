2808d218d6a10806ebd383d898d06581e25df93a
Julius Schulz <julius.schulz@fu-berlin.de>
2012-12-14 02:42:55 +0100

    added GPL header

--- pattern.py
+++ pattern.py
@@ -1,16 +1,43 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import sys
 import warnings
 import copy
 import scipy.optimize
 
+"""
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-#a*l**2+b*l+c=d
-def solve_quadratic(a,b,c,d):
-    cc = c-d
-    return np.array([(-b + np.sqrt(b**2 - 4*a*cc))/(2*a),(-b - np.sqrt(b**2 - 4*a*cc))/(2*a)])
-    
-    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Dieses Programm ist Freie Software: Sie können es unter den Bedingungen
+    der GNU General Public License, wie von der Free Software Foundation,
+    Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren
+    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+    Dieses Programm wird in der Hoffnung, dass es nützlich sein wird, aber
+    OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+    Siehe die GNU General Public License für weitere Details.
+
+    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+"""
+
+
+#def solve_quadratic(a,b,c,d):
+#    cc = c-d
+#    return np.array([(-b + np.sqrt(b**2 - 4*a*cc))/(2*a),(-b - np.sqrt(b**2 - 4*a*cc))/(2*a)])
 
 class VectorLine:
 
